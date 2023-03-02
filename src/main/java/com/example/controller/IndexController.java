1790dfa2f052d20b914301ea5dc3a60605ddabb9
fanny <fanny2519@163.com>
2023-03-02 17:37:47 +0800

    修改地图

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.bean.*;
 import com.example.service.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
